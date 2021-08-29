66edbcd9ff040f3b5804bb90bc6794c6e72cf9a0
Guy <shefer.guy1@gmail.com>
2021-08-29 14:31:35 +0300

    add app templates

--- projects/views.py
+++ projects/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse('projects')
+    # return HttpResponse('projects')
+    return render(request, 'projects/projects.html')
 
 
 def single_project(request, pk):
-    return HttpResponse('single project ' + str(pk))
+    # return HttpResponse('single project ' + str(pk))
+    return render(request, 'projects/single-project.html')
